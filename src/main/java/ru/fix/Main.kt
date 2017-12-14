3730cc3920aab0e818b084b5deb1bfce3901764b
Marat Gizatullin <maratische@gmail.com>
2017-12-14 23:56:15 +0300

    один раз вызываем shuffle, стало более лучше распределять, но все равно писать свой вариант случайной выборки и нужно учитывать запреты

--- src/main/java/ru/fix/Main.kt
+++ src/main/java/ru/fix/Main.kt
@@ -1,14 +1,18 @@
 package ru.fix
 
 import com.fasterxml.jackson.databind.DeserializationFeature
-import com.fasterxml.jackson.module.kotlin.*
-import org.jetbrains.annotations.Nullable
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import com.fasterxml.jackson.module.kotlin.readValue
 import java.io.File
 import java.time.DayOfWeek
 import java.time.LocalDate
 import java.util.*
 import kotlin.collections.ArrayList
-import kotlin.collections.HashMap
+
+open class Day (val day: LocalDate, var name: String?);
+class WorkDay(day: LocalDate) : Day(day, null);
+class DayOffAM(day: LocalDate) : Day(day, null);
+class DayOffPM(day: LocalDate) : Day(day, null);
 
 data class Config(val persons: List<Person>, val month: Int, val year: Int, val selebrations : List<Int>?)
 
@@ -26,41 +30,35 @@ fun main(args: Array<String>) {
     val config = convert(jsonFile)
     println(config)
     //получаем коллекцию рабочих дней и выходных
-    val workDays = ArrayList<LocalDate>()
-    val dayOffDays = ArrayList<LocalDate>()
+    val workDays = ArrayList<Day>()
     var firstDay = LocalDate.of(config.year, config.month,1);
     do {
         if (firstDay.dayOfWeek in listOf<DayOfWeek>(DayOfWeek.SATURDAY, DayOfWeek.SUNDAY)) {
-            dayOffDays.add(firstDay)
+            workDays.add(DayOffAM(firstDay))
+            workDays.add(DayOffPM(firstDay))
         } else if (config.selebrations != null && firstDay.dayOfMonth in config.selebrations) {
-            dayOffDays.add(firstDay)
+            workDays.add(DayOffAM(firstDay))
+            workDays.add(DayOffPM(firstDay))
         } else {
-            workDays.add(firstDay)
+            workDays.add(WorkDay(firstDay))
         }
         firstDay = firstDay.plusDays(1);
 
     } while (firstDay.dayOfMonth != 1)
 
-    var schedule = shufflePeople(workDays, config)
-    var scheduleDay1 = shufflePeople(dayOffDays, config)
-    var scheduleDay2 = shufflePeople(dayOffDays, config)
-    for (day in scheduleDay2.keys.sorted()) {
-        schedule.put(day, scheduleDay1.get(day)+"/"+scheduleDay2.get(day))
+    val peoples = ArrayList<String>()
+    for ((index,day) in workDays.withIndex()) {
+        peoples.add(config.persons.get(index % config.persons.size).name)
     }
 
-    for (day in schedule.keys.sorted()) {
-        println("${day} - ${schedule.get(day)}")
+    Collections.shuffle(peoples);
+
+    for ((index,day) in workDays.withIndex()) {
+        day.name = peoples.get(index)
     }
 
-}
-
-private fun shufflePeople(workDays: ArrayList<LocalDate>, config: Config): HashMap<LocalDate, String> {
-    Collections.shuffle(workDays);
-    var schedule = HashMap<LocalDate, String>()
-    var index = 0;
     for (day in workDays) {
-        val name = config.persons.get(index++ % config.persons.size).name
-        schedule.put(day, name)
+        println("${day.day} - ${day.name}")
     }
-    return schedule
 }
+
